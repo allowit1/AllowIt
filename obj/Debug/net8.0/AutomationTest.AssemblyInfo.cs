7d9cf81c96dc6a82e79b5a14259a9d2a8b794dae
Benaya Trabelsi <btrabels@g.jct.ac.il>
2024-07-17 22:18:42 +0300

    Your commit message here

--- obj/Debug/net8.0/AutomationTest.AssemblyInfo.cs
+++ obj/Debug/net8.0/AutomationTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutomationTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b624623e1450e2d3a13b59406cb498882a21f35a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76855adcfc53187a13c99fb55d42418c5dd16d3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutomationTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutomationTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
